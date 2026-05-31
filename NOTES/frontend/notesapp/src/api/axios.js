08dc2909ccb8de73ca008720629148bc4d728320
Arjun_2025 <vineetaarjun123@gmail.com>
2026-05-31 21:50:21 +0530

    deployment completed

--- NOTES/frontend/notesapp/src/api/axios.js
+++ NOTES/frontend/notesapp/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  "https://dev-projects-73iz.onrender.com",
+  baseURL:  "https://dev-projects-73iz.onrender.com/notes",
 });
 
 api.interceptors.response.use(
